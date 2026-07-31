73fcdfdcebd542bd9cb65ca0e3583857d8af4761
strikeouts27 <99090208+strikeouts27@users.noreply.github.com>
2026-07-30 19:03:22 -0500

    auth0 developer mode code included. with this code we can have a developer mode that can be an additional login and we are able to see changes on the webpage for different employee roles. We can see what the manager sees, and what a regular employee sees. we are not forced to sign into each role and develop and sign out.
    see the appsettings.json and helper folder code.
    
    Add development auth mode with fake login handler
    
    Introduce UseFakeDevLogin setting to enable bypassing Auth0 authentication for local development. Register DevelopmentAuthHandler when enabled, providing a hardcoded developer identity and role. Update Program.cs for conditional registration and add DevelopmentAuthHandler.cs. Minor formatting changes in the project file.

--- Program.cs
+++ Program.cs
@@ -1,7 +1,9 @@
+using Auth0.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.EntityFrameworkCore;
 using ScrumMovieTheater.Data;
+using ScrumMovieTheater.Helpers;
 using ScrumMovieTheater.Services;
-using Auth0.AspNetCore.Authentication; 
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -19,12 +21,26 @@ builder.Services.AddSession(options =>
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddAuth0WebAppAuthentication(options =>
+// PLEASE NOTE THAT THIS DEVELOPER MODE WILL RUN ON BREAKPOINTS AND THAT YOU NEED TO TURN THE DEVELOPER MODE ON AND OFF WITH TRUE FALSE VALUES IN APP SETTINGS JSON. 
+
+// the development auth handler is this class that we wrote in the Helpers folder under the file DevelopmentAuthHandler
+
+if (builder.Configuration["UseFakeDevLogin"].Equals("true", StringComparison.CurrentCultureIgnoreCase))
 {
-    options.Domain = builder.Configuration["Auth0:Domain"];
-    options.ClientId = builder.Configuration["Auth0:ClientId"];
-    options.ClientSecret = builder.Configuration["Auth0:ClientSecret"];
-});
+    builder.Services.AddAuthentication("DevelopmentScheme")
+    .AddScheme<AuthenticationSchemeOptions, DevelopmentAuthHandler>("DevelopmentScheme", null);
+    builder.Services.AddAuthorization();
+}
+else
+{
+    /* https://manage.auth0.com/dashboard/us/dev-iy52fklqvibb5b8m/applications/f8isqivRoiCb2ZckyBchJMRzzHXLFtPN/quickstart/webapp/aspnet-core */
+    builder.Services.AddAuth0WebAppAuthentication(options =>
+    {
+        options.Domain = builder.Configuration["Auth0:Domain"];
+        options.ClientId = builder.Configuration["Auth0:ClientId"];
+        options.ClientSecret = builder.Configuration["Auth0:ClientSecret"];
+    });
+}
 
 
 // Add DbContext HERE
